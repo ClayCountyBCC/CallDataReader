ebd3e3b76b94a17b9f8428564935e433eb1d20d3
Daniel McCartney <daniel.mccartney@claycountygov.com>
2020-03-02 06:20:33 -0500

    initial connection to postgresql database and BaseData model completed. Improved ErrorLog added.

---
+++ Models/BaseData.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using Npgsql;
+using System.Data;
+
+namespace CallDataReader.Models
+{
+  public class BaseData
+  {
+    public long call_id { get; set; } = -1;
+    public string session_id { get; set; } = "";
+    public DateTime call_origin_time { get; set; } = DateTime.MinValue;
+    public string call_type { get; set; } = "";
+    public string agency_name { get; set; } = "";
+    public long location_id { get; set; } = -1;
+    public string phone_number { get; set; } = "";
+    public string street_number { get; set; } = "";
+    public string street_name { get; set; } = "";
+    public string city { get; set; } = "";
+    public string caller_state { get; set; } = "";
+    public string call_location_type { get; set; } = "";
+    public string caller_name { get; set; } = "";
+    public string caller_location { get; set; } = "";
+    public string location_confidence { get; set; } = "";
+
+    public BaseData() { }
+
+    public static List<BaseData> Get(string cs, long previous_max_id = -1)
+    {
+      if (previous_max_id == -1) previous_max_id = Get_Previous_Max_Id();
+      var param = new DynamicParameters();
+      param.Add(":previous_max_id", previous_max_id);
+
+      string query = @"
+        SELECT 
+          S.""SourceRecordID"" call_id
+          ,S.""solacom-session-id"" session_id
+          ,S.""solacom-origin-time"" call_origin_time
+          ,S.""solacom-callType"" call_type
+          ,S.""solacom-agencyOrElement"" agency_name
+          ,A.""SourceRecordID"" location_id
+          ,A.""Attach1"" phone_number
+          ,A.""Attach2"" street_number
+          ,A.""Attach3"" street_name
+          ,A.""Attach4"" city
+          ,A.""Attach5"" caller_state
+          ,A.""Attach7"" call_location_type
+          ,A.""Attach8"" caller_name
+          ,A.""Attach16"" caller_location
+          ,A.""Attach17"" location_confidence
+        FROM PUBLIC.""SolacomCDRv10"" S
+        INNER JOIN PUBLIC.""Attachments"" A ON S.""FileID"" = A.""FileID""
+        WHERE
+          S.""SourceRecordID"" > :previous_max_id
+          AND S.""solacom-eventType"" = 'ALIresponse'
+        ORDER BY S.""solacom-session-id"" ASC, S.""SourceRecordID"" DESC";
+
+      try
+      {
+        using (IDbConnection db = new NpgsqlConnection(cs))
+        {
+          return (List<BaseData>)db.Query<BaseData>(query, param);
+        }
+      }
+      catch(Exception ex)
+      {
+        new ErrorLog(ex);
+        return null;
+      }
+      
+
+    }
+
+    private static long Get_Previous_Max_Id()
+    {
+      return 7145348;
+      // this function 
+    }
+
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+using System.IO;
+using CallDataReader.Models;
 
 namespace CallDataReader
 {
@@ -6,7 +10,23 @@ namespace CallDataReader
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+      var config = builder.Build();
+
+      string call_db_cs = config.GetConnectionString("CallDB");
+      string tracking_db_cs = config.GetConnectionString("TrackingDB");
+
+      var data = BaseData.Get(call_db_cs);
+
+      var max = (from d in data
+                 select d.call_id).Max();
+
+      Console.WriteLine(max.ToString());
+      // don't store WPH1 / WPH2 addresses
+
     }
   }
 }
